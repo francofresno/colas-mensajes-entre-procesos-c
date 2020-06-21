d1102196e061f0fb887d260fa2ba8fceca68bf63
jftierno <jftierno@gmail.com>
2020-06-20 21:52:27 -0300

    Inicio de manejo de datos de pokemons

--- GameCard/src/filesystem.c
+++ GameCard/src/filesystem.c
@@ -1,9 +1,10 @@
 #include "filesystem.h"
 
 char* directoriosACrear[] = {"Metadata", "Files", "Blocks"};
-char* BLOCKS = "5192";
-char* BLOCK_SIZE = "64";
-char* MAGIC_NUMBER = "TALL_GRASS";
+int BLOCKS;
+int BLOCK_SIZE;
+
+
 t_bitarray* bitarray;
 
 char* obtenerRutaTotal(char* path)
@@ -24,11 +25,15 @@ void configuracionInicial(void)
 	}
 }
 
+/******************************************
+ **Funciones de verificación de archivos***
+ ******************************************/
+
 void verificarDirectorio(char* path)
 {
 	DIR* directorioActual = opendir(path);
 
-	if(directorioActual == NULL)
+	if(!directorioActual)
 			mkdir(path, 0777);
 
 	free(directorioActual);
@@ -54,38 +59,97 @@ void verificarMetadata(char* path)
 	if(strcmp(path, "Metadata") == 0)
 	{
 		t_config* config = config_create(pathActual);
+		BLOCKS = config_get_int_value(config, "BLOCKS");
+		BLOCK_SIZE = config_get_int_value(config, "BLOCK_SIZE");
 		verificarBitmap(pathDirectorio, config);
 		config_destroy(config);
 	}
-	else if(strcmp(path, "Bloques") == 0){
-
+	else if(strcmp(path, "Blocks") == 0){
+		verificarBloques(pathDirectorio);
 	}
 }
 
 void verificarBitmap(char* pathActual, t_config* config)
 {
 	string_append(&pathActual, "/Bitmap.bin");
-	int cantidadBloques = config_get_int_value(config, "BLOCKS");
+	char* bitarrayFlujo;
 
 	FILE* archivoActual = fopen(pathActual, "rb");
 	if(!archivoActual)
-	{
 		archivoActual = fopen(pathActual, "wb");
-		char* seteoInicial = string_repeat('0', cantidadBloques);
-		fprintf(archivoActual, "%s", seteoInicial);
-		fclose(archivoActual);
-	}
-
-	char* bitarrayFlujo = malloc(cantidadBloques);
-	archivoActual = fopen(pathActual, "rb");
-	fread(bitarrayFlujo, 1, cantidadBloques, archivoActual);
-	bitarray = bitarray_create_with_mode(bitarrayFlujo, cantidadBloques/8, LSB_FIRST);
-	if(!bitarray_test_bit(bitarray, 3))
-		printf("Dio false");
-
 	fclose(archivoActual);
+
+	truncate(pathActual, BLOCKS/8);
+	int fileDescriptor = open(pathActual, O_RDWR);
+	bitarrayFlujo = mmap(NULL, BLOCKS/8, PROT_WRITE | PROT_READ, MAP_SHARED, fileDescriptor, 0);
+	bitarray = bitarray_create_with_mode(bitarrayFlujo, BLOCKS/8, LSB_FIRST);
+//	msync((void*) bitarrayFlujo, BLOCKS/8, MS_SYNC);
+	close(fileDescriptor);
 }
-//void verificarBloques(char* path)
-//{
-////	for(int i; i < atoi(BLOCKS))
-//}
+
+void verificarBloques(char* pathDirectorio)
+{
+	FILE* archivoActual;
+	for(int i = 1; i <= BLOCKS; i++)
+	{
+		char* numeroBloque = malloc(7);
+		char* pathActual = strdup(pathDirectorio);
+		sprintf(numeroBloque, "/%d.bin", i);
+		string_append(&pathActual, numeroBloque);
+		archivoActual = fopen(pathActual, "rb");
+		if(!archivoActual)
+		{
+			archivoActual = fopen(pathActual, "wb");
+			truncate(pathActual, BLOCK_SIZE);
+		}
+		fclose(archivoActual);
+		free(numeroBloque);
+		free(pathActual);
+	}
+}
+
+char* verificarPokemon(t_nombrePokemon nombrePokemon)
+{
+	char* pathActual = obtenerRutaTotal("/Files/");
+	string_append(&pathActual, nombrePokemon.nombre);
+	FILE* archivoActual = fopen(pathActual, "rb");
+	if(!archivoActual)
+		archivoActual = fopen(pathActual, "wb");
+	fclose(archivoActual);
+	return pathActual;
+}
+
+/******************************************
+ **Funciones de modificación de archivos***
+ ******************************************/
+
+void modificarMetadataPokemon(char* pathDirectorio)
+{
+	char* pathMetadata = pathDirectorio;
+	string_append(&pathMetadata, "/Metadata.bin");
+
+	FILE* archivoActual = fopen(pathMetadata, "rb");
+	if(!archivoActual)
+	{
+		archivoActual = fopen(pathMetadata, "wb");
+		t_config* config = config_create(pathMetadata);
+		config_set_value(config, "DIRECTORY", "N");
+		asignarBloque();
+		//		config_set_value(config, "SIZE", size);
+		//		config_set_value(config, "BLOCKS", bloques);
+	}
+}
+char** asignarBloque()
+{
+	for(int i = 0; i < BLOCKS; i++)
+	{
+		if(!bitarray_test_bit(bitarray, i))
+		{
+			bitarray_set_bit(bitarray, i);
+
+		}
+	}
+}
+
+
+
